482779e4c9a0d88dada7963ff6385f45b9283cc4
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-05-07 13:28:10 +0500

    content added

--- src/template/product.js
+++ src/template/product.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from 'react';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export default function Product ({pageContext}) {
 
-    console.log(pageContext)
-    
+export default function Product({pageContext}){
+
+    const {itemDetails} = pageContext
     return (
         <div>
-       <h1>Title</h1>
+        <h1>{itemDetails.title}</h1>
+        <p>{documentToReactComponents(itemDetails.desc.json)}</p>
         </div>
-    )
+   )
 }
